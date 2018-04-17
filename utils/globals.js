a1d90b9ac5d8efd6f4b6441003fef1942c8f2813
Harkeerat Toor <hadou798@gmail.com>
2018-04-16 21:17:55 -0700

    Change toolbar color

--- utils/globals.js
+++ utils/globals.js
@@ -1,6 +1,6 @@
 export default {
   COLOR: {
-    TOOLBAR_COLOR: "#0F3274",
+    TOOLBAR_COLOR: "#455ac9",
     TOOLBAR_COLOR_ALT: "#DEBB0A",
     TOOLBAR_COLOR_ALT2: "#003436",
     TOOLBAR_TINT: "#fff"
