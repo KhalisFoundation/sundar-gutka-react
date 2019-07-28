3ece41cfb9c2c4f652443fdd3981b99ce3d30237
Harkeerat Toor <hadou798@gmail.com>
2019-07-28 00:37:44 -0700

    Updating to RN .6 (in progress)

--- android/app/src/main/java/com/WahegurooNetwork/SundarGutka/MainApplication.java
+++ android/app/src/main/java/com/WahegurooNetwork/SundarGutka/MainApplication.java
@@ -2,27 +2,15 @@ package com.WahegurooNetwork.SundarGutka;
 
 import android.app.Application;
 
-import com.apsl.versionnumber.RNVersionNumberPackage;
-import com.corbt.keepawake.KCKeepAwakePackage;
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.zmxv.RNSound.RNSoundPackage;
 
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import org.pgsqlite.SQLitePluginPackage;
-
-import java.util.Arrays;
 import java.util.List;
 
-import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
-import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
@@ -36,22 +24,14 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-              new MainReactPackage(),
-            new RNSoundPackage(),
-            new RNVersionNumberPackage(),
-            new VectorIconsPackage(),
-            new SplashScreenReactPackage(),
-            new RNSpinkitPackage(),
-            new KCKeepAwakePackage(),
-            new RNFirebasePackage(),
-              new RNFirebaseCrashlyticsPackage(),
-            new RNGestureHandlerPackage(),
-              new RNFirebaseAnalyticsPackage(),
-              new RNFirebaseMessagingPackage(),
-              new RNFirebaseNotificationsPackage(),
-              new SQLitePluginPackage() // register SQLite Plugin here
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here:
+      packages.add(new RNFirebaseAnalyticsPackage());
+      packages.add(new RNFirebaseMessagingPackage());
+      packages.add(new RNFirebaseNotificationsPackage());
+
+      return packages;
     }
 
     @Override
