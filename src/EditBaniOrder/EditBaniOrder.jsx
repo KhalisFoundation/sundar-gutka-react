4c18afed14a86394978085e17ad99c3f8d4d8aca
Amitoj Singh <amitojsingh95@gmail.com>
2023-08-25 10:55:28 -0600

    inProgress: editBani Order

--- src/EditBaniOrder/EditBaniOrder.jsx
+++ src/EditBaniOrder/EditBaniOrder.jsx
@@ -1,3 +1,11 @@
+import React, { useEffect } from "react";
 import DraggableFlatList from "react-native-draggable-flatlist";
-function EditBaniOrder() {}
+import { useSelector } from "react-redux";
+
+function EditBaniOrder() {
+  const { transliterationLanguage } = useSelector((state) => state);
+  useEffect(() => {
+    (async () => {})();
+  }, [transliterationLanguage]);
+}
 export default EditBaniOrder;
