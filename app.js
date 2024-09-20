2e90aec9180d21f95db6db0cf0a7d46dd4e71f82
Amitoj Singh <amitojsingh95@gmail.com>
2024-09-20 09:44:39 -0600

    fix: loader

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ import { PersistGate } from "reduxjs-toolkit-persist/integration/react";
 import ErrorBoundary from "react-native-error-boundary";
 import { createStore, errorHandler, FallBack } from "@common";
 import Navigation from "./src/navigation";
-import Loader from "./src/Loader/Loader";
 
 const { store, persistor } = createStore();
 function App() {
   return (
     <ErrorBoundary onError={errorHandler} FallbackComponent={FallBack}>
       <Provider store={store}>
-        <PersistGate loading={<Loader />} persistor={persistor}>
+        <PersistGate loading={null} persistor={persistor}>
           <Navigation />
         </PersistGate>
       </Provider>
