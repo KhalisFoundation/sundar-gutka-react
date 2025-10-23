30092a1d3f6e85239471ad802b6a1e115a56a63b
Amitoj Singh <amitojsingh95@gmail.com>
2025-10-23 11:40:17 -0600

    Remove hardcoded BASIC_AUTH_PASSWORD in constant.js for enhanced security

--- src/common/constant.js
+++ src/common/constant.js
@@ -91,7 +91,7 @@ export default {
   LANDSCAPE: "LANDSCAPE",
   REMOTE_DB_URL: "https://banidb.blob.core.windows.net/database",
   BASIC_AUTH_USERNAME: "admin",
-  BASIC_AUTH_PASSWORD: "sttm_audio_api@Khalis13",
+  BASIC_AUTH_PASSWORD: "",
   REMOTE_AUDIO_API_URL:
     "https://sttm-audio-api-v2.salmonriver-80392db4.eastus.azurecontainerapps.io/v1/",
   CHOPAYI_SAHIB_ID: 9,
